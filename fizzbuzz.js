d1bbf1285b400d9d61b7026b83bd00fae02dc607
Robin Blake <robinblake@ntlworld.com>
2025-07-01 17:01:19 +0100

    implemented basic fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,11 @@
 for (let i = 1; i <= 100; i++) {
-    console.log(i.toString());
+    if (i % 15 == 0) {
+        console.log("Fizzbuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i.toString());
+    }
 }
