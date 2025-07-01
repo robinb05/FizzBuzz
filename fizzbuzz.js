b0d253d72789e239f943454f14fcde56c87cfffd
Robin Blake <robinblake@ntlworld.com>
2025-07-01 17:25:21 +0100

    Added rule for 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,18 @@
 for (let i = 1; i <= 100; i++) {
     let result = "";
+    let set13 = false;
 
-    // classic fizzbuzz conditions
-    if (i % 15 == 0) {
-        result = "Fizzbuzz";
-    } else if (i % 3 == 0) {
-        result = "Fizz";
-    } else if (i % 5 == 0) {
-        result = "Buzz";
+    if (i % 3 == 0) {
+        result += "Fizz";
+    }
+
+    if (i % 13 == 0) {
+        set13 = true;
+        result += "Fezz"
+    }
+
+    if (i % 5 == 0) {
+        result += "Buzz";
     }
 
     // multiple of 7 part
@@ -15,15 +20,19 @@ for (let i = 1; i <= 100; i++) {
         result += "Bang";
     }
     
-    // override for multiple of 11
+    // 11 overwrites all updates apart the possible Fezz from 13
     if (i % 11 == 0) {
-        result = "Bong";
+        if (set13) {
+            result = "FezzBong"
+        } else {
+            result = "Bong";
+        }
     }
 
-    // print either number or new string
-    if (result != "") {
-        console.log(result);
-    } else {
-        console.log(i.toString());
+    // if no specific string, then give a number
+    if (result == "") {
+        result = i.toString();
     }
+
+    console.log(result);
 }
