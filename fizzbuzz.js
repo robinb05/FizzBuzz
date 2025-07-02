dee9dfa916e53b2933413811789b3d80214aeb7e
Robin Blake <robinblake@ntlworld.com>
2025-07-02 09:18:11 +0100

    Added the remaining number rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,38 +1,54 @@
-for (let i = 1; i <= 100; i++) {
-    let result = "";
+function fizzBuzz(num) {
+        
+    const result = [];
     let set13 = false;
 
-    if (i % 3 == 0) {
-        result += "Fizz";
+    if (num % 3 == 0) {
+        result.push("Fizz");
     }
 
-    if (i % 13 == 0) {
+    if (num % 13 == 0) {
         set13 = true;
-        result += "Fezz"
+        result.push("Fezz");
     }
 
-    if (i % 5 == 0) {
-        result += "Buzz";
+    if (num % 5 == 0) {
+        result.push("Buzz");
     }
 
-    // multiple of 7 part
-    if (i % 7 == 0) {
-        result += "Bang";
+    if (num % 7 == 0) {
+        result.push("Bang");
     }
-    
+
     // 11 overwrites all updates apart the possible Fezz from 13
-    if (i % 11 == 0) {
+    if (num % 11 == 0) {
+        // clear the array
+        result.length = 0;
         if (set13) {
-            result = "FezzBong"
+            result.push("FezzBong");
         } else {
-            result = "Bong";
+            result.push("Bong");
         }
     }
 
-    // if no specific string, then give a number
-    if (result == "") {
-        result = i.toString();
+    // 17 reverses the order of words
+    if (num % 17 == 0) {
+        result.reverse();
     }
 
-    console.log(result);
+    let result_str = "";
+    // if no specific string, then give a number, otherwise get the string from the array
+    if (result.length == 0) {
+        result_str = num.toString();
+    } else {
+        for (let i=0; i < result.length; i++) {
+            result_str += result[i];
+        }
+    }
+
+    return result_str;
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(fizzBuzz(i))
 }
